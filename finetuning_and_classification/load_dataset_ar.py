b6976bf7b58f5ac940ab98a3a1eeb53872bcde16
pucse <phamuyen.cse@gmail.com>
2021-03-18 09:45:03 +0700

    no message

--- finetuning_and_classification/load_dataset_ar.py
+++ finetuning_and_classification/load_dataset_ar.py
@@ -110,7 +110,7 @@ class ARDataset(datasets.GeneratorBasedBuilder):
             {
                 "sentence": datasets.Value("string"),
                 "label": datasets.Sequence(
-                    feature = datasets.Value("int"), 
+                    feature = datasets.Value("int8"), 
                     length=len(_ASPECTS)
                 )
                 #These are the features of your dataset like images, labels ...
